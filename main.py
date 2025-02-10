2def3d9bf497bdb6649ab6ec281baba36bfeb62a
dipesh paneru <dipeshpaneru1997@gmail.com>
2025-02-10 14:43:59 -0500

    Initial setup

--- main.py
+++ main.py
@@ -13,9 +13,71 @@ def getDataFromCsv(file_path, column1_index, column2_index):
                 print(f"ERROR: Error while accessing data from CSV file")
     return data
 
+def cleanUpData(data):
+    data.pop(0)
+
+    for x in data:
+        if x[0] == "":
+            data.remove(x)
+            
+    return data
 
 
 data = getDataFromCsv("Cancer_dataset.csv", 4, 33)
-data.pop(0)
+DATA = cleanUpData(data)
+
+def performLinearRegression():
+
+    B0 = 0.2
+    B1 = 0.2
+
+    iterate(B0, B1)
+
+
+def getFeatures():
+    features = [float(x[0]) for x in DATA]
+    return features
+
+def getOutputs():
+    output = [float(x[1]) for x in DATA]
+    return output
+
+
+def computePredictedOutput(B0, B1):
+    predictedOutputs = []
+
+    for x in getFeatures():
+        y = B0 + (B1 * x)
+        predictedOutputs.append(y)
+
+    return predictedOutputs
+
+def calculateCost(predictedOutput, actualOutput):
+
+    total = 0
+
+    result = (predictedOutput - actualOutput) ** 2
+    total += result
+
+    cost = total / 2
+    return cost
+
+
+def iterate(B0, B1):
+    predictedOutputs = computePredictedOutput(B0, B1)
+    outputs = getOutputs()
+    costs = []
+
+    for y in predictedOutputs:
+        cost = calculateCost(y, outputs.pop(0))
+        costs.append(cost)
+
+    print(getOutputs)
+    print(costs)
+
+   
+
+performLinearRegression()
+
 
 
