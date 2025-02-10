67421d884458f6b596b37c57df1df106cb84e502
dipesh paneru <dipeshpaneru1997@gmail.com>
2025-02-10 13:36:21 -0500

    Get data from csv

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+
+def getDataFromCsv(file_path, column1_index, column2_index):
+    
+    data = []
+    with open(file_path, 'r') as file:
+        
+        reader = csv.reader(file)
+        for row in reader:
+            try:
+                data.append((row[column1_index], row[column2_index]))
+            except:
+                print(f"ERROR: Error while accessing data from CSV file")
+    return data
+
+
+
+data = getDataFromCsv("Cancer_dataset.csv", 4, 33)
+data.pop(0)
+
