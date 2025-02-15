29457d7c7cbbd7b83e22b817c13863a9c0cfc7aa
dipesh paneru <dipeshpaneru1997@gmail.com>
2025-02-15 13:00:39 -0500

    Conversion form float to Decimal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import random
+from decimal import Decimal
 
 # Linear regression model Class with all the components required for linear regression
 class LinearRegression:
@@ -22,7 +23,7 @@ class LinearRegression:
 
     def getOutputs(self, data):
         if data != None:
-            output = [float(x[1]) for x in data]
+            output = [Decimal(x[1]) for x in data]
             return output
 
     def performLinearRegression(self):
@@ -32,10 +33,10 @@ class LinearRegression:
         predictedOutputs = []
 
         for x in self.getFeatures2D(data):
-            y = thetas[0]
+            y = Decimal(thetas[0])
 
             for idx, i in enumerate(x):
-                y += (float(thetas[idx + 1]) * float(i))
+                y += (Decimal(thetas[idx + 1]) * Decimal(i))
             
             predictedOutputs.append(y)
 
@@ -51,7 +52,7 @@ class LinearRegression:
             result = (x - outputs[idx]) ** 2
             total += result
 
-        cost = total / float((2 * featureLen))
+        cost = total / Decimal((2 * featureLen))
         return cost
     
 
@@ -59,7 +60,7 @@ class LinearRegression:
         features = self.getFeatures2D(self.trainingData)
         outputs = self.getOutputs(self.trainingData)
 
-        featureLen = float(len(features))
+        featureLen = Decimal(len(features))
         
         total = 0
 
@@ -68,11 +69,11 @@ class LinearRegression:
             if isIntercept:
                 result = (predictedOutputs[idx] - outputs[idx])
             else:
-                result = (predictedOutputs[idx] - outputs[idx]) * float(x[index])
+                result = (predictedOutputs[idx] - outputs[idx]) * Decimal(x[index])
 
             total += result
         
-        change = (self.lr * total) / featureLen
+        change = (Decimal(self.lr) * total) / featureLen
 
         newB = B - change
         return newB
@@ -196,65 +197,65 @@ trainingData = dataEx.getReqColsFromData([3] ,TRAINING_DATA)
 testData = dataEx.getReqColsFromData([3], TEST_DATA)
 
 
-lr2 = LinearRegression(trainingData, testData, 1000, [0, 0], 0.001)
+lr2 = LinearRegression(trainingData, testData, 1000, [0, 0], Decimal("0.001"))
 lr2.performLinearRegression()
 
 # print("\n---------------- Question 2 -------------")
 
-trainingData2 = dataEx.getReqColsFromData([3, 32] ,TRAINING_DATA)
-testData2 = dataEx.getReqColsFromData([3, 32], TEST_DATA)
+# trainingData2 = dataEx.getReqColsFromData([3, 32] ,TRAINING_DATA)
+# testData2 = dataEx.getReqColsFromData([3, 32], TEST_DATA)
 
-lr2 = LinearRegression(trainingData2, testData2, 1000, [0, 0, 0], 0.001)
-lr2.performLinearRegression()
+# lr2 = LinearRegression(trainingData2, testData2, 1000, [0, 0, 0], 0.001)
+# lr2.performLinearRegression()
 
 
-print("\n---------------- Question 3 -------------")
-print("\nStep 1")
-# # I have chosen lymph_node_status first because in question 2
-# # adding lymph_node_status to the model seemed to better the model performance
+# print("\n---------------- Question 3 -------------")
+# print("\nStep 1")
+# # # I have chosen lymph_node_status first because in question 2
+# # # adding lymph_node_status to the model seemed to better the model performance
 
-trainingData3 = dataEx.getReqColsFromData([10] ,TRAINING_DATA)
-testData3 = dataEx.getReqColsFromData([10], TEST_DATA)
+# trainingData3 = dataEx.getReqColsFromData([10] ,TRAINING_DATA)
+# testData3 = dataEx.getReqColsFromData([10], TEST_DATA)
 
-lr3 = LinearRegression(trainingData3, testData3, 1000, [0, 0], 0.001)
-lr3.performLinearRegression()
+# lr3 = LinearRegression(trainingData3, testData3, 1000, [0, 0], 0.001)
+# lr3.performLinearRegression()
 
-print("\nStep 2")
-# # I have chosen mean_radius in the for second step,
-# # because radius should impact the size 
+# print("\nStep 2")
+# # # I have chosen mean_radius in the for second step,
+# # # because radius should impact the size 
 
-trainingData4 = dataEx.getReqColsFromData([32, 10] ,TRAINING_DATA)
-testData4 = dataEx.getReqColsFromData([32, 10], TEST_DATA)
+# trainingData4 = dataEx.getReqColsFromData([32, 10] ,TRAINING_DATA)
+# testData4 = dataEx.getReqColsFromData([32, 10], TEST_DATA)
 
-lr4 = LinearRegression(trainingData4, testData4, 1000, [0, 0, 0], 0.001)
-lr4.performLinearRegression()
+# lr4 = LinearRegression(trainingData4, testData4, 1000, [0, 0, 0], 0.001)
+# lr4.performLinearRegression()
 
-print("\nStep 3")
-#  # In third step I am adding mean_smoothness because mean_radius, 
-#  # mean_parameter and mean_area seems to have some colinearity, thus I am choosing 
-#  # something that is not these two
+# print("\nStep 3")
+# #  # In third step I am adding mean_smoothness because mean_radius, 
+# #  # mean_parameter and mean_area seems to have some colinearity, thus I am choosing 
+# #  # something that is not these two
 
-trainingData5 = dataEx.getReqColsFromData([2, 10, 32] ,TRAINING_DATA)
-testData5 = dataEx.getReqColsFromData([2, 10, 32], TEST_DATA)
+# trainingData5 = dataEx.getReqColsFromData([2, 10, 32] ,TRAINING_DATA)
+# testData5 = dataEx.getReqColsFromData([2, 10, 32], TEST_DATA)
 
-lr5 = LinearRegression(trainingData5, trainingData5, 1000, [0, 0, 0, 0], 0.001)
-lr5.performLinearRegression()
+# lr5 = LinearRegression(trainingData5, trainingData5, 1000, [0, 0, 0, 0], 0.001)
+# lr5.performLinearRegression()
 
-print("\nStep 4")
+# print("\nStep 4")
 
-trainingData6 = dataEx.getReqColsFromData([2, 6, 10, 32] ,TRAINING_DATA)
-testData6 = dataEx.getReqColsFromData([2, 6, 10, 32], TEST_DATA)
+# trainingData6 = dataEx.getReqColsFromData([2, 6, 10, 32] ,TRAINING_DATA)
+# testData6 = dataEx.getReqColsFromData([2, 6, 10, 32], TEST_DATA)
 
-lr6 = LinearRegression(trainingData6, trainingData6, 1000, [0, 0, 0, 0, 0], 0.001)
-lr6.performLinearRegression()
+# lr6 = LinearRegression(trainingData6, trainingData6, 1000, [0, 0, 0, 0, 0], 0.001)
+# lr6.performLinearRegression()
 
-print("\nStep 5")
+# print("\nStep 5")
 
-trainingData7 = dataEx.getReqColsFromData([2, 6, 10, 32, 11] ,TRAINING_DATA)
-testData7 = dataEx.getReqColsFromData([2, 6, 10, 32, 11], TEST_DATA)
+# trainingData7 = dataEx.getReqColsFromData([2, 6, 10, 32, 11] ,TRAINING_DATA)
+# testData7 = dataEx.getReqColsFromData([2, 6, 10, 32, 11], TEST_DATA)
 
-lr7 = LinearRegression(trainingData7, testData7, 1000, [0, 0, 0, 0, 0, 0], 0.001)
-lr7.performLinearRegression()
+# lr7 = LinearRegression(trainingData7, testData7, 1000, [0, 0, 0, 0, 0, 0], 0.001)
+# lr7.performLinearRegression()
 
 # print("\nStep 6")
 
